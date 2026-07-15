6d1670cb3ec4350d1f2c11257c633d6a7e486cfd
Augusto Alves <augustojbe@gmail.com>
2026-07-14 23:08:13 -0300

    add github actions workflow

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Se o repositório no GitHub se chamar 'clima', o base deve ser '/clima/'
+  // Se for uma User Page (ex: username.github.io) ou tiver domínio customizado, altere para '/'
+  base: '/clima/',
+});
